6b681c818a92eafa64d1d82283e66104ebf14281
Pedro Lopes <83576881+pedrompflopes@users.noreply.github.com>
2022-11-07 12:07:34 +0000

    make git extension not required (#261)
    * make git extension not required
    
    * add log messages

--- src/extension.ts
+++ src/extension.ts
@@ -40,7 +40,6 @@ import { REFRESH_TREE } from "./utils/common/commands";
 import {disableButton, enableButton, getChanges} from "./utils/utils";
 import { KicsProvider } from "./utils/kics/kics_provider";
 import { applyScaFix } from "./utils/scaFix";
-import { GitExtension } from "./types/git";
 import { getLearnMore } from "./utils/sast/learnMore";
 import {getAstConfiguration, isScanRunning, pollForScan, updateStatusBarItem,isScanEnabled} from "./utils/ast/ast";
 import {cancelScan, createScan} from "./create_scan_provider";
@@ -219,11 +218,17 @@ export async function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(newDetails);
   
   // Branch Listener
-  const gitExtension = vscode.extensions.getExtension<GitExtension>('vscode.git')!.exports;
-  if (gitExtension.enabled) {
-    context.subscriptions.push(await getBranchListener(context, logs));
+  const gitExtension = vscode.extensions.getExtension('vscode.git');
+  if (gitExtension) {
+    await gitExtension.activate();
+    if (gitExtension && gitExtension.exports.enabled) {
+      logs.info("Git Extension - Add branch.");
+      context.subscriptions.push(await getBranchListener(context, logs));
+    } else {
+      logs.warn("Git Extension - Could not find active git extension in workspace.");
+    }
   } else {
-    logs.warn("Could not find active git extension in workspace, will not listen to branch changes");
+    logs.warn("Git extension - Could not find vscode.git installed.");
   }
 
   // Settings
