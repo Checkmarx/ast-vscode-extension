6881646b7e2806f4c3cbf6741d3c22061c65e7c4
tiagobcx <tiago.baptista@checkmarx.com>
2022-11-09 17:23:32 -0600

    trying test fix

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -191,9 +191,9 @@ describe("UI tests", async function () {
     // const fileWorkspaceNotifications = await new Workbench().getNotifications();
     // const fileWorkspaceNotification = fileWorkspaceNotifications[0];
     // await fileWorkspaceNotification.takeAction(YES);
-    // await delay(FIFTY_SECONDS);
-    // await delay(FIFTY_SECONDS);
-    // await delay(FIFTY_SECONDS);
+    await delay(FIFTY_SECONDS);
+    await delay(FIFTY_SECONDS);
+    await delay(FIFTY_SECONDS);
     const resultsNotifications = await new Workbench().getNotifications();
     const firstNotification = resultsNotifications[0];
     const title = await firstNotification.getMessage();
