6401d0ca21200afeea8b423ae02e10f241f138d2
tiagobcx <tiago.baptista@checkmarx.com>
2022-12-13 16:56:11 +0000

    rearrange tests

--- src/test/5.WebViewDetails.test.ts
+++ src/test/5.WebViewDetails.test.ts
@@ -1,7 +1,7 @@
-import { By, CustomTreeSection, EditorView, InputBox, StatusBar, until, VSBrowser, WebDriver, WebView, Workbench} from 'vscode-extension-tester';
+import { By, CustomTreeSection, Editor, EditorView, InputBox, StatusBar, until, VSBrowser, WebDriver, WebView, Workbench} from 'vscode-extension-tester';
 import { expect } from 'chai';
 import { delay, getDetailsView, getQuickPickSelector, getResults, initialize, quickPickSelector } from './utils';
-import { CX_CLEAR, CX_LOOK_SCAN, CX_SELECT_ALL, CX_SELECT_BRANCH, CX_SELECT_PROJECT, CX_SELECT_SCAN, CX_TEST_SCAN_PROJECT_NAME, VS_CLOSE_GROUP_EDITOR, VS_OPEN_FOLDER } from './constants';
+import { CX_CLEAR, CX_LOOK_SCAN, CX_SELECT_ALL, CX_SELECT_BRANCH, CX_SELECT_PROJECT, CX_SELECT_SCAN, CX_TEST_SCAN_PROJECT_NAME, FIVE_SECONDS, THIRTY_SECONDS, THREE_SECONDS, VS_CLOSE_GROUP_EDITOR, VS_OPEN_FOLDER } from './constants';
 
 describe('WebView results detail test', () => {
 	let bench: Workbench;
@@ -21,6 +21,15 @@ describe('WebView results detail test', () => {
     });
 
 	it("should check open webview and codebashing link", async function () {
+		
+		driver.wait(
+			until.elementLocated(
+			  By.className(
+				"monaco-tl-twistie codicon codicon-tree-item-expanded collapsible collapsed"
+			  )
+			),
+			15000
+		  );
 		treeScans = await initialize();
 		while(treeScans===undefined){
 			treeScans = await initialize();
@@ -35,19 +44,15 @@ describe('WebView results detail test', () => {
 		}		
 		// Get results and open details page
 		let sastNode = await scan?.findChildItem("sast");
-		while (sastNode===undefined) {
-			sastNode = await scan?.findChildItem("sast");
-		}
 		let result = await getResults(sastNode);
-		while(result===undefined){
-			result = await getResults(sastNode);
-		}
+		await delay(THIRTY_SECONDS);
 		let resultName = await result[0].getLabel();
+		await delay(FIVE_SECONDS);
 		await result[0].click();
-		
+		await delay(THIRTY_SECONDS);
 		// Close left view
 		let leftView = new WebView();
-		await delay(5000);
+		await delay(THIRTY_SECONDS);
 		await leftView.click();
 		await bench.executeCommand(VS_CLOSE_GROUP_EDITOR);
 		// Open details view
@@ -56,19 +61,19 @@ describe('WebView results detail test', () => {
 		let titleWebElement = await detailsView.findWebElement(
 		  By.id("cx_title")
 		);
-		
+		await delay(FIVE_SECONDS);
 		let title = await titleWebElement.getText();
-		
+		await delay(FIVE_SECONDS);
 		expect(title).to.equal(resultName);
 		let codebashingWebElement = await detailsView.findWebElement(
 		  By.id("cx_header_codebashing")
 		);
-		
+		await delay(FIVE_SECONDS);
 		let codebashing = await codebashingWebElement.getText();
-		
+		await delay(FIVE_SECONDS);
 		expect(codebashing).is.not.undefined;
 		await detailsView.switchBack();
-		
+		await delay(THREE_SECONDS);		
 	});
 
   it("should click on comments", async function () {
