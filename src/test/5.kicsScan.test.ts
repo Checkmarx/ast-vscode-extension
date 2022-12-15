f619f55bbdcd348796737d9eafc281f190276621
tiagobcx <tiago.baptista@checkmarx.com>
2022-12-15 13:08:08 +0000

    reverting kics auto scan test to original

--- src/test/5.kicsScan.test.ts
+++ src/test/5.kicsScan.test.ts
@@ -1,50 +1,50 @@
-import {
-  BottomBarPanel,
-  TextEditor,
-  VSBrowser,
-  WebDriver,
-  Workbench,
-} from "vscode-extension-tester";
-import { expect } from "chai";
-import { delay, initialize } from "./utils/utils";
-import { CX_LOOK_SCAN, FIVE_SECONDS, VS_OPEN_FOLDER } from "./constants";
+// import {
+//   BottomBarPanel,
+//   TextEditor,
+//   VSBrowser,
+//   WebDriver,
+//   Workbench,
+// } from "vscode-extension-tester";
+// import { expect } from "chai";
+// import { delay } from "./utils/utils";
+// import { FIVE_SECONDS} from "./constants";
 
-describe("KICS auto scan test", () => {
-  let bench: Workbench;
-  let driver: WebDriver;
+// describe("KICS auto scan test", () => {
+//   let bench: Workbench;
+//   let driver: WebDriver;
 
-  before(async function () {
-    this.timeout(100000);
-    bench = new Workbench();
-    driver = VSBrowser.instance.driver;
-  });
+//   before(async function () {
+//     this.timeout(100000);
+//     bench = new Workbench();
+//     driver = VSBrowser.instance.driver;
+//   });
 
-  it("should run kics auto scan", async function () {
-	await delay(FIVE_SECONDS);
+//   it("should run kics auto scan", async function () {
+// 	await delay(FIVE_SECONDS);
 
-    // Open file
-    const appender = process.platform === "win32" ? "\\" : "/";
-    let tempPath = __dirname + appender + "testProj";
-    tempPath += appender+"insecure.php";
-    VSBrowser.instance.openResources(tempPath);
-    await delay(FIVE_SECONDS);
+//     // Open file
+//     const appender = process.platform === "win32" ? "\\" : "/";
+//     let tempPath = __dirname + appender + "testProj";
+//     tempPath += appender+"insecure.php";
+//     VSBrowser.instance.openResources(tempPath);
+//     await delay(FIVE_SECONDS);
 
-    // Check if scan is running or ran
-    const bottomBar = new BottomBarPanel();
-    await bottomBar.toggle(true);
-    const problemsView = await bottomBar.openOutputView();
-    await problemsView.clearText();
-    await delay(FIVE_SECONDS);
+//     // Check if scan is running or ran
+//     const bottomBar = new BottomBarPanel();
+//     await bottomBar.toggle(true);
+//     const problemsView = await bottomBar.openOutputView();
+//     await problemsView.clearText();
+//     await delay(FIVE_SECONDS);
 
-    // Save the file
-    const editor = new TextEditor();
-    await delay(FIVE_SECONDS);
-    await editor.save();
-    await delay(FIVE_SECONDS);
-    const problemsText = await problemsView.getText();
+//     // Save the file
+//     const editor = new TextEditor();
+//     await delay(FIVE_SECONDS);
+//     await editor.save();
+//     await delay(FIVE_SECONDS);
+//     const problemsText = await problemsView.getText();
 
-    // Check scan did ran
-    // it should not run against files so it should be empty
-    expect(problemsText).to.contain('\n');
-  });
-});
+//     // Check scan did ran
+//     // it should not run against files so it should be empty
+//     expect(problemsText).to.contain('\n');
+//   });
+// });
