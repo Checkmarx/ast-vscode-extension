91ec050acde566c610a6855acd8f3b6c801bdc07
tiagobcx <tiago.baptista@checkmarx.com>
2023-01-06 13:32:36 +0000

    adding new test case for SCA panel

---
+++ src/test/8.scaResults.test.ts
@@ -0,0 +1,49 @@
+import {
+	BottomBarPanel,
+	CustomTreeSection,
+	EditorView,
+	VSBrowser,
+	WebDriver,
+	Workbench,
+  } from "vscode-extension-tester";
+  import { expect } from "chai";
+  import { initializeSCA } from "./utils/utils";
+  import {
+	CX_CLEAR,
+	CX_SCA_SCAN,
+  } from "./utils/constants";
+  
+  describe("SCA scan panel test", () => {
+	let bench: Workbench;
+	let tree: CustomTreeSection;
+	let driver: WebDriver;
+  
+	before(async function () {
+	  this.timeout(100000);
+	  bench = new Workbench();
+	  driver = VSBrowser.instance.driver;
+	});
+  
+	after(async () => {
+	  await new EditorView().closeAllEditors();
+	  await bench.executeCommand(CX_CLEAR);
+	});
+  
+	it("should check if tree and play button exists", async function () {
+	  tree = await initializeSCA();
+	  expect(tree).is.not.undefined;
+	});
+
+	it("should check if tree and play button exists", async function () {
+		tree = await initializeSCA();
+		await bench.executeCommand(CX_SCA_SCAN);
+		let outputView = await new BottomBarPanel().openOutputView();
+		let text = await outputView.getText();
+		while(!text.includes("Scan completed successfully")){
+			outputView = await new BottomBarPanel().openOutputView();
+			text = await outputView.getText();
+		}
+		expect(text).is.not(undefined);
+	  });
+  });
+  
