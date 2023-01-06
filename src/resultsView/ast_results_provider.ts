4c72dbc19e17e5c45d5223213ed5bc78b0e3f2c9
tiagobcx <tiago.baptista@checkmarx.com>
2023-01-06 11:20:58 +0000

    code refactor to share tree functions between results provider

--- src/resultsView/ast_results_provider.ts
+++ src/resultsView/ast_results_provider.ts
@@ -1,9 +1,7 @@
 import * as vscode from "vscode";
 import * as fs from "fs";
 import * as path from "path";
-import CxResult from "@checkmarxdev/ast-cli-javascript-wrapper/dist/main/results/CxResult";
 import { EventEmitter } from "vscode";
-import { AstResult } from "../models/results";
 import {
   EXTENSION_NAME,
   IssueFilter,
@@ -18,18 +16,15 @@ import {
   PROJECT_ITEM,
   BRANCH_ITEM,
   SCAN_ITEM,
-  GRAPH_ITEM,
   REFRESHING_TREE,
 } from "../utils/common/constants";
-import {
-  Counter,
-  getProperty, getResultsFilePath, getResultsWithProgress,
-} from "../utils/utils";
+import { getResultsFilePath, getResultsWithProgress } from "../utils/utils";
 import { Logs } from "../models/logs";
 import { get, update } from "../utils/common/globalState";
 import { getAstConfiguration } from "../utils/ast/ast";
-import { SastNode } from "../models/sastNode";
 import { REFRESH_TREE } from "../utils/common/commands";
+import { TreeItem } from "../utils/tree/treeItem";
+import { createSummaryItem, groupBy, orderResults } from "../utils/tree/actions";
 
 
 export class AstResultsProvider implements vscode.TreeDataProvider<TreeItem> {
@@ -133,147 +128,17 @@ export class AstResultsProvider implements vscode.TreeDataProvider<TreeItem> {
     this.scan = get(this.context, SCAN_ID_KEY)?.id;
     if (fs.existsSync(resultJsonPath) && this.scan) {
       const jsonResults = JSON.parse(fs.readFileSync(resultJsonPath, "utf-8").replace(/:([0-9]{15,}),/g, ':"$1",'));
-      const results = this.orderResults(jsonResults.results);
+      const results = orderResults(jsonResults.results);
 
-      treeItems = treeItems.concat(this.createSummaryItem(results));
+      treeItems = treeItems.concat(createSummaryItem(results));
 
-      const treeItem = this.groupBy(results, this.issueFilter);
+      const treeItem = groupBy(results, this.issueFilter,this.scan,this.diagnosticCollection);
       treeItem.label = `${SCAN_LABEL} ${this.scan}`;
       treeItem.collapsibleState = vscode.TreeItemCollapsibleState.Expanded;
       treeItems = treeItems.concat(treeItem);
     }
 
     return new TreeItem("", undefined, undefined, treeItems);
-  }
-
-  orderResults(list: CxResult[]): CxResult[] {
-    const order = ["HIGH", "MEDIUM", "LOW", "INFO"];
-    return list.sort(
-      (a, b) => order.indexOf(a.severity) - order.indexOf(b.severity)
-    );
-  }
-
-  createSummaryItem(list: CxResult[]): TreeItem {
-    const counter = new Counter(list, (p: CxResult) => p.severity);
-    const label = Array.from(counter.keys())
-      .map((key) => `${key}: ${counter.get(key)}`)
-      .join(" | ");
-    return new TreeItem(label, GRAPH_ITEM, undefined);
-  }
-
-  groupBy(list: Object[], groups: string[]): TreeItem {
-    const folder = vscode.workspace.workspaceFolders?.[0];
-    const map = new Map<string, vscode.Diagnostic[]>();
-    const tree = new TreeItem(this.scan ?? "", undefined, undefined, []);
-    list.forEach((element:any) =>{
-      this.groupTree(element, folder, map, groups, tree);
-    }
-    );
-
-    this.diagnosticCollection.clear();
-    map.forEach((value, key) =>
-      this.diagnosticCollection.set(vscode.Uri.parse(key), value)
-    );
-
-    return tree;
-  }
-
-  groupTree(
-    rawObj: Object,
-    folder: vscode.WorkspaceFolder | undefined,
-    map: Map<string, vscode.Diagnostic[]>,
-    groups: string[],
-    tree: TreeItem
-  ) {
-    const obj = new AstResult(rawObj);
-    if (!obj) {
-      return;
-    }
-    const item = new TreeItem(obj.label.replaceAll("_", " "), undefined, obj);
-    let node;
-    // Verify the current severity filters applied
-    if (this.issueLevel.length > 0) {
-      // Filter only the results for the severity and state filters type
-      if (
-        this.issueLevel.includes(obj.getSeverity()) &&
-        this.stateLevel.includes(obj.getState()!)
-      ) {
-        if (obj.sastNodes.length > 0) {
-          this.createDiagnostic(
-            obj.label,
-            obj.getSeverityCode(),
-            obj.sastNodes[0],
-            folder,
-            map
-          );
-        }
-        node = groups.reduce(
-          (previousValue: TreeItem, currentValue: string) =>
-            this.reduceGroups(obj, previousValue, currentValue),
-          tree
-        );
-        node.children?.push(item);
-      }
-    }
-  }
-
-  createDiagnostic(
-    label: string,
-    severity: vscode.DiagnosticSeverity,
-    node: SastNode,
-    folder: vscode.WorkspaceFolder | undefined,
-    map: Map<string, vscode.Diagnostic[]>
-  ) {
-    if (!folder) {
-      return;
-    }
-    const filePath = vscode.Uri.joinPath(folder!.uri, node.fileName).toString();
-    // Needed because vscode uses zero based line number
-    const column = node.column > 0 ? +node.column - 1 : 1;
-    const line = node.line > 0 ? +node.line - 1 : 1;
-    let length = column + node.length;
-    const startPosition = new vscode.Position(line, column);
-    const endPosition = new vscode.Position(line, length);
-    const range = new vscode.Range(startPosition, endPosition);
-
-    const diagnostic = new vscode.Diagnostic(range, label, severity);
-    if (map.has(filePath)) {
-      map.get(filePath)?.push(diagnostic);
-    } else {
-      map.set(filePath, [diagnostic]);
-    }
-  }
-
-  reduceGroups(obj: any, previousValue: TreeItem, currentValue: string) {
-    var value = getProperty(obj, currentValue);
-    
-    // Needed to group by filename in kics, in case nothing is found then its a kics result and must be found inside data.filename
-    if (currentValue === IssueFilter.fileName && value.length === 0) {
-      value = getProperty(obj.data, IssueFilter.fileName.toLowerCase());
-    }
-    
-    if (!value) {
-      return previousValue;
-    }
-
-    const tree = previousValue.children
-      ? previousValue.children.find(
-          (item) => item.label === value.replaceAll("_", " ")
-        )
-      : undefined;
-    if (tree) {
-      tree.setDescription();
-      return tree;
-    }
-
-    const newTree = new TreeItem(
-      value.replaceAll("_", " "),
-      undefined,
-      undefined,
-      []
-    );
-    previousValue.children?.push(newTree);
-    return newTree;
   }
 
   getTreeItem(element: TreeItem): vscode.TreeItem | Thenable<vscode.TreeItem> {
@@ -287,58 +152,5 @@ export class AstResultsProvider implements vscode.TreeDataProvider<TreeItem> {
       return this.data;
     }
     return element.children;
-  }
-}
-
-export class TreeItem extends vscode.TreeItem {
-  children: TreeItem[] | undefined;
-  result: AstResult | undefined;
-  size: number;
-
-  constructor(
-    label: string,
-    type?: string,
-    result?: AstResult,
-    children?: TreeItem[]
-  ) {
-    super(
-      label,
-      children === undefined
-        ? vscode.TreeItemCollapsibleState.None
-        : vscode.TreeItemCollapsibleState.Collapsed
-    );
-    this.result = result;
-    this.size = 1;
-    this.contextValue = type;
-    this.children = children;
-    // TODO: Use a type enum
-     if (type) {
-      this.iconPath = new vscode.ThemeIcon("shield");
-    }
-
-    if (type) {
-      this.iconPath = new vscode.ThemeIcon("shield");
-    }
-    if (type === GRAPH_ITEM) {
-      this.iconPath = new vscode.ThemeIcon("graph");
-    }
-    if (type === PROJECT_ITEM) {
-      this.iconPath = new vscode.ThemeIcon("project");
-    }
-    if (type === BRANCH_ITEM) {
-      this.iconPath = new vscode.ThemeIcon("repo");
-    }
-    if (result) {
-      this.iconPath = result.getTreeIcon();
-    }
-  }
-
-  setDescription() {
-    +this.size++;
-    this.description = "" + this.size;
-  }
-
-  setDescriptionValue(description: string) {
-    this.description = description;
   }
 }
--- src/scaView/sca_results_provider.ts
+++ src/scaView/sca_results_provider.ts
@@ -1,23 +1,18 @@
 import * as vscode from "vscode";
 import CxResult from "@checkmarxdev/ast-cli-javascript-wrapper/dist/main/results/CxResult";
 import { EventEmitter } from "vscode";
-import { AstResult } from "../models/results";
 import {
   EXTENSION_NAME,
   IssueFilter,
-  PROJECT_ITEM,
-  BRANCH_ITEM,
-  GRAPH_ITEM,
-  SCA_NO_VULNERABILITIES,
   REFRESHING_TREE,
   CLEAR_SCA,
   SCA_SCAN_RUNNING_LOG,
   SCA_START_SCAN,
 } from "../utils/common/constants";
-import { Counter, getProperty } from "../utils/utils";
 import { Logs } from "../models/logs";
 import { getAstConfiguration } from "../utils/ast/ast";
-import { SastNode } from "../models/sastNode";
+import { TreeItem } from "../utils/tree/treeItem";
+import { groupBy, orderResults } from "../utils/tree/actions";
 
 export class SCAResultsProvider implements vscode.TreeDataProvider<TreeItem> {
   public process: any;
@@ -74,143 +69,20 @@ export class SCAResultsProvider implements vscode.TreeDataProvider<TreeItem> {
   generateTree(message?:string): TreeItem {
     this.diagnosticCollection.clear();
     let treeItems = [];
-    const results = this.orderResults(this.scaResults);
+    const results = orderResults(this.scaResults);
     let treeItem: TreeItem;
     if (results.length > 0) {
       let today = new Date();
       this.scan = `Latest scan: ${ today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()+" "+today.getHours()+":"+today.getMinutes()}`;
-      treeItem = this.groupBy(results, this.issueFilter);
+      treeItem = groupBy(results, this.issueFilter,this.scan,this.diagnosticCollection);
       treeItem.collapsibleState = vscode.TreeItemCollapsibleState.Expanded;
     } else {
       this.scan = message?message:SCA_START_SCAN;
-      treeItem = this.groupBy(results, this.issueFilter);
+      treeItem = groupBy(results, this.issueFilter,this.scan,this.diagnosticCollection);
       treeItem.collapsibleState = vscode.TreeItemCollapsibleState.None;
     }
     treeItems = treeItems.concat(treeItem);
     return new TreeItem("", undefined, undefined, treeItems);
-  }
-
-  orderResults(list: CxResult[]): CxResult[] {
-    const order = ["HIGH", "MEDIUM", "LOW", "INFO"];
-    return list.sort(
-      (a, b) => order.indexOf(a.severity) - order.indexOf(b.severity)
-    );
-  }
-
-  createSummaryItem(list: CxResult[]): TreeItem {
-    const counter = new Counter(list, (p: CxResult) => p.severity);
-    const label = Array.from(counter.keys())
-      .map((key) => `${key}: ${counter.get(key)}`)
-      .join(" | ");
-    return new TreeItem(label, GRAPH_ITEM, undefined);
-  }
-
-  groupBy(list: Object[], groups: string[]): TreeItem {
-    const folder = vscode.workspace.workspaceFolders?.[0];
-    const map = new Map<string, vscode.Diagnostic[]>();
-
-    const tree = this.scan? new TreeItem(this.scan, undefined, undefined, []):undefined;
-    list.forEach((element: any) => {
-      this.groupTree(element, folder, map, groups, tree);
-    });
-
-    this.diagnosticCollection.clear();
-    map.forEach((value, key) =>
-      this.diagnosticCollection.set(vscode.Uri.parse(key), value)
-    );
-
-    return tree;
-  }
-
-  groupTree(
-    rawObj: Object,
-    folder: vscode.WorkspaceFolder | undefined,
-    map: Map<string, vscode.Diagnostic[]>,
-    groups: string[],
-    tree: TreeItem
-  ) {
-    const obj = new AstResult(rawObj);
-    if (!obj) {
-      return;
-    }
-    const item = new TreeItem(obj.label.replaceAll("_", " "), undefined, obj);
-    let node;
-    // Verify the current severity filters applied
-
-    if (obj.sastNodes.length > 0) {
-      this.createDiagnostic(
-        obj.label,
-        obj.getSeverityCode(),
-        obj.sastNodes[0],
-        folder,
-        map
-      );
-    }
-    node = groups.reduce(
-      (previousValue: TreeItem, currentValue: string) =>
-        this.reduceGroups(obj, previousValue, currentValue),
-      tree
-    );
-    node.children?.push(item);
-  }
-
-  createDiagnostic(
-    label: string,
-    severity: vscode.DiagnosticSeverity,
-    node: SastNode,
-    folder: vscode.WorkspaceFolder | undefined,
-    map: Map<string, vscode.Diagnostic[]>
-  ) {
-    if (!folder) {
-      return;
-    }
-    const filePath = vscode.Uri.joinPath(folder!.uri, node.fileName).toString();
-    // Needed because vscode uses zero based line number
-    const column = node.column > 0 ? +node.column - 1 : 1;
-    const line = node.line > 0 ? +node.line - 1 : 1;
-    let length = column + node.length;
-    const startPosition = new vscode.Position(line, column);
-    const endPosition = new vscode.Position(line, length);
-    const range = new vscode.Range(startPosition, endPosition);
-
-    const diagnostic = new vscode.Diagnostic(range, label, severity);
-    if (map.has(filePath)) {
-      map.get(filePath)?.push(diagnostic);
-    } else {
-      map.set(filePath, [diagnostic]);
-    }
-  }
-
-  reduceGroups(obj: any, previousValue: TreeItem, currentValue: string) {
-    var value = getProperty(obj, currentValue);
-
-    // Needed to group by filename in kics, in case nothing is found then its a kics result and must be found inside data.filename
-    if (currentValue === IssueFilter.fileName && value.length === 0) {
-      value = getProperty(obj.data, IssueFilter.fileName.toLowerCase());
-    }
-
-    if (!value) {
-      return previousValue;
-    }
-
-    const tree = previousValue.children
-      ? previousValue.children.find(
-          (item) => item.label === value.replaceAll("_", " ")
-        )
-      : undefined;
-    if (tree) {
-      tree.setDescription();
-      return tree;
-    }
-
-    const newTree = new TreeItem(
-      value.replaceAll("_", " "),
-      undefined,
-      undefined,
-      []
-    );
-    previousValue.children?.push(newTree);
-    return newTree;
   }
 
   getTreeItem(element: TreeItem): vscode.TreeItem | Thenable<vscode.TreeItem> {
@@ -225,57 +97,7 @@ export class SCAResultsProvider implements vscode.TreeDataProvider<TreeItem> {
     }
     return element.children;
   }
+  
 }
 
-export class TreeItem extends vscode.TreeItem {
-  children: TreeItem[] | undefined;
-  result: AstResult | undefined;
-  size: number;
 
-  constructor(
-    label: string,
-    type?: string,
-    result?: AstResult,
-    children?: TreeItem[]
-  ) {
-    super(
-      label,
-      children === undefined
-        ? vscode.TreeItemCollapsibleState.None
-        : vscode.TreeItemCollapsibleState.Collapsed
-    );
-    this.result = result;
-    this.size = 1;
-    this.contextValue = type;
-    this.children = children;
-    // TODO: Use a type enum
-    if (type) {
-      this.iconPath = new vscode.ThemeIcon("shield");
-    }
-
-    if (type) {
-      this.iconPath = new vscode.ThemeIcon("shield");
-    }
-    if (type === GRAPH_ITEM) {
-      this.iconPath = new vscode.ThemeIcon("graph");
-    }
-    if (type === PROJECT_ITEM) {
-      this.iconPath = new vscode.ThemeIcon("project");
-    }
-    if (type === BRANCH_ITEM) {
-      this.iconPath = new vscode.ThemeIcon("repo");
-    }
-    if (result) {
-      this.iconPath = result.getTreeIcon();
-    }
-  }
-
-  setDescription() {
-    +this.size++;
-    this.description = "" + this.size;
-  }
-
-  setDescriptionValue(description: string) {
-    this.description = description;
-  }
-}
