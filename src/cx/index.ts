625f3bc6b7b942915b4d030147e20d452b030f36
tiagobcx <tiago.baptista@checkmarx.com>
2023-05-22 12:02:48 +0100

    merge with main

--- src/cx/index.ts
+++ src/cx/index.ts
@@ -2,4 +2,4 @@ import *  as prod from "./cx";
 import *  as testEnv from "./cxMock";
 
 export const cx =
-  process.env.TEST &&  process.env.TEST === 'true' ? new testEnv.CxMock : new prod.Cx;
+  process.env.TEST && process.env.TEST === 'true' ? new testEnv.CxMock : new prod.Cx;
