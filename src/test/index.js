5d65a53963c6900a96708c78df229f1de13922f5
tiagobcx <tiago.baptista@checkmarx.com>
2023-07-27 17:46:55 +0100

    removing js

--- src/test/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const vscode_extension_tester_1 = require("vscode-extension-tester");
-const testExtensionsDir = 'out/test/';
-const vscodeVersion = vscode_extension_tester_1.ReleaseQuality.Insider;
-const tester = new vscode_extension_tester_1.ExTester(undefined, vscodeVersion, testExtensionsDir);
-tester.setupAndRunTests(testExtensionsDir);
-//# sourceMappingURL=index.js.map
