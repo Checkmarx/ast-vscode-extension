582ee758fc70bd6433af36d9f9dd5ad5e309a407
tamarleviCm <tamar.levi@checkmarx.com>
2024-07-09 16:08:48 +0300

    tests

--- src/test/9.vorpal.test.ts
+++ src/test/9.vorpal.test.ts
@@ -2,6 +2,7 @@ import { BottomBarPanel, EditorView, SettingsEditor, VSBrowser, WebDriver, Workb
 import { expect } from "chai";
 import { initialize } from "./utils/utils";
 import { waitStatusBar } from "./utils/waiters";
+import { CX_CATETORY } from "./utils/constants";
 
 describe("Vorpal engine tets", () => {
   let settingsEditor: SettingsEditor;
@@ -25,7 +26,7 @@ describe("Vorpal engine tets", () => {
     settingsEditor = await bench.openSettings();
     const vorpalToggle = await settingsEditor.findSetting(
       "Activate Vorpal Auto Scanning",
-      "CheckmarxVorpal"
+      CX_CATETORY
     );
     expect(vorpalToggle).to.not.be.undefined;
   });
@@ -33,7 +34,7 @@ describe("Vorpal engine tets", () => {
     settingsEditor = await bench.openSettings();
     const vorpalToggle = await settingsEditor.findSetting(
       "Activate Vorpal Auto Scanning",
-      "CheckmarxVorpal"
+      CX_CATETORY
     );
     await vorpalToggle.setValue("true");
     const vorpalToggleValue = await vorpalToggle.getValue();
@@ -42,7 +43,12 @@ describe("Vorpal engine tets", () => {
   });
   it("vorpal starts when the apikey changes", async function () {});
   it("vorpal stops listening when Vorpal is turned off in settings", async function () {});
-  it("vorpal scan is triggered when a file is edited", async function () {});
+  it("vorpal scan is triggered when a file is edited", async function () {
+    const editor = new EditorView();
+    await editor.openEditor("package.json");
+    await editor.sendKeys("abc");
+
+  });
   it("vorpal scan is triggered when a file is opened", async function () {});
   it("vorpal scan is not triggered when vorpal is turned off and the file is edited", async function () {});
   it("vorpal scan with an unsupported language", async function () {});
