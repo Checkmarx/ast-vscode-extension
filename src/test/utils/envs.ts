816783fdf8eb518459b926042035bb511372f39d
sarahCx <sarah.chen@checkmarx.com>
2024-07-17 11:35:50 +0300

    Test to create scan with no results (AST-48230) (#891)
    * don't faild if there are no old scan results
    
    * add test to create scan with no results
    
    ---------
    
    Co-authored-by: Or Shamir Checkmarx <93518641+OrShamirCM@users.noreply.github.com>

--- src/test/utils/envs.ts
+++ src/test/utils/envs.ts
@@ -1,3 +1,4 @@
 export const API_KEY: string = "1";
 export const SCAN_ID: string = "1";
 export const CX_TEST_SCAN_PROJECT_NAME: string = "test-proj-21";
+export const EMPTY_RESULTS_SCAN_ID: string = "3";
