fc2c1df2908472d70dd86a862fc4721b81ad5c10
tamarleviCm <tamar.levi@checkmarx.com>
2024-07-21 16:11:44 +0300

    try

--- src/extension.ts
+++ src/extension.ts
@@ -219,11 +219,5 @@ export async function activate(context: vscode.ExtensionContext) {
 
 }
 
-exports.activate = activate;
-
 export function deactivate() {}
 
-module.exports = {
-  activate,
-  deactivate
-};
