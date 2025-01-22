fbff9ae6054cb0237f1e65368ba0499b5cddad98
ItayPaz <143506741+Korjen97@users.noreply.github.com>
2025-01-22 10:29:35 +0200

    other/Add test with real data and added command to run pipline (AST-80487) (#1031)
    * other/Add test with real data and added command to run pipline
    
    * fix yml file
    
    * fix test
    
    * add api key value in pipline
    
    * fix yml
    
    * fix secrets

--- src/cx/index.ts
+++ src/cx/index.ts
@@ -1,5 +1,10 @@
-import *  as prod from "./cx";
-import *  as testEnv from "./cxMock";
-
+import * as prod from "./cx";
+import * as testEnv from "./cxMock";
+ 
+//prettier-ignore
 export const cx =
-  process.env.TEST && process.env.TEST === 'true' ? new testEnv.CxMock : new prod.Cx;
+  process.env.TEST && process.env.TEST === "true"
+    ? new testEnv.CxMock
+    : process.env.TEST === "uiEndToEnd"
+    ? new prod.Cx
+    : new prod.Cx;
---
+++ src/e2e/utils/constants.ts
@@ -0,0 +1,3 @@
+export const CX_TEST_PROJECT_NAME = "elchananTEST";
+export const CX_TEST_BRANCH_NAME = "main";
+export const CX_TEST_SCAN_NAME = "latest";
