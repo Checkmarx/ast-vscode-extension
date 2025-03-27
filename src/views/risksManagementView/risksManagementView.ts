c812faf09498d16805ff8737bda7b425c7370207
galactica <tiago.baptista@checkmarx.com>
2025-03-27 15:16:17 +0200

    Fix style and small bugs.

--- src/views/risksManagementView/risksManagementView.ts
+++ src/views/risksManagementView/risksManagementView.ts
@@ -4,110 +4,142 @@ import { RisksManagementService } from "./risksManagementService";
 import { Item } from "../../utils/common/globalState";
 
 export class RisksManagementView implements vscode.WebviewViewProvider {
-  private _view?: vscode.WebviewView;
-  private _risksManagementService: RisksManagementService;
-  constructor(
-    private readonly _extensionUri: vscode.Uri,
-    private readonly context: vscode.ExtensionContext
-  ) {
-    this._risksManagementService = RisksManagementService.getInstance(
-      this.context
-    );
-  }
-
-  public resolveWebviewView(
-    webviewView: vscode.WebviewView,
-    _context: vscode.WebviewViewResolveContext,
-    _token: vscode.CancellationToken
-  ) {
-    this._view = webviewView;
-
-    webviewView.webview.options = {
-      enableScripts: true,
-      localResourceRoots: [this._extensionUri],
-    };
-
-    this.updateContent();
-  }
-
-  public async updateContent(project?: Item, scan?: Item) {
-    if (!this._view) {
-      return;
+    private _view?: vscode.WebviewView;
+    private _risksManagementService: RisksManagementService;
+    constructor(
+        private readonly _extensionUri: vscode.Uri,
+        private readonly context: vscode.ExtensionContext
+    ) {
+        this._risksManagementService = RisksManagementService.getInstance(
+            this.context
+        );
     }
-    const webview = this._view.webview;
-    if (!project && !scan) {
-      this._view.webview.html = this.getWebviewContent(
-        undefined,
-        undefined,
-        false,
-        { applicationNameIDMap: [] }
-      );
-      return;
+
+    public resolveWebviewView(
+        webviewView: vscode.WebviewView,
+        _context: vscode.WebviewViewResolveContext,
+        _token: vscode.CancellationToken
+    ) {
+        this._view = webviewView;
+
+        webviewView.webview.options = {
+            enableScripts: true,
+            localResourceRoots: [this._extensionUri],
+        };
+
+        this.updateContent();
     }
-    const isLatestScan = await this._risksManagementService.checkIfLatestScan(
-      project.id,
-      scan.id
-    );
-    const projectToDisplay = exctarctData(project.name, "Project:");
-    const scanToDisplay = exctarctData(scan.displayScanId, "Scan:");
-    const riskManagementResults =
-      await this._risksManagementService.getRiskManagementResults(project.id);
 
-    this._view.webview.html = this.getWebviewContent(
-      projectToDisplay,
-      scanToDisplay,
-      isLatestScan,
-      riskManagementResults as { applicationNameIDMap: any[] }
-    );
-    this._view.webview.postMessage({
-      command: "getRiskManagementResults",
-      data: riskManagementResults,
-    });
-  }
-  private setWebUri(...paths: string[]): vscode.Uri {
-    return this._view.webview.asWebviewUri(
-      vscode.Uri.joinPath(this.context.extensionUri, ...paths)
-    );
-  }
+    public async updateContent(project?: Item, scan?: Item) {
+        if (!this._view) {
+            return;
+        }
+        this._view.webview.postMessage({ command: "showLoader" });
 
-  private getWebviewContent(
-    projectName: string,
-    scan: string,
-    isLatestScan: boolean,
-    applications: { applicationNameIDMap: any[] }
-  ): string {
-    const styleResetUri = this.setWebUri("media", "reset.css");
-    const styleVSCodeUri = this.setWebUri("media", "vscode.css");
-    const styleMainUri = this.setWebUri("media", "main.css");
-    const scriptUri = this.setWebUri("media", "risksManagement.js");
-    const styleUri = this.setWebUri("media", "risksManagement.css");
-    const unionIcon = this.setWebUri("media", "icons", "union.svg");
-    const codiconsUri = this.setWebUri(
-      "node_modules",
-      "@vscode/codicons",
-      "dist",
-      "codicon.css"
-    );
-    const popperUri = this.setWebUri(
-      "node_modules",
-      "@popperjs/core",
-      "dist",
-      "umd",
-      "popper.min.js"
-    );
-    const styleBootStrap = this.setWebUri(
-      "media",
-      "bootstrap",
-      "bootstrap.min.css"
-    );
-    const scriptBootStrap = this.setWebUri(
-      "media",
-      "bootstrap",
-      "bootstrap.min.js"
-    );
-    const nonce = getNonce();
+        if (!project && !scan) {
+            this._view.webview.html = this.getWebviewContent(
+                undefined,
+                undefined,
+                false,
+                { applicationNameIDMap: [] }
+            );
+            this._view.webview.postMessage({ command: "hideLoader" });
+            return;
+        }
+        try {
+            const isLatestScan = await this._risksManagementService.checkIfLatestScan(
+                project.id,
+                scan.id
+            );
+            const projectToDisplay = exctarctData(project.name, "Project:");
+            const scanToDisplay = exctarctData(scan.displayScanId, "Scan:");
+            const riskManagementResults =
+                await this._risksManagementService.getRiskManagementResults(project.id);
 
-    return `<!DOCTYPE html>
+            this._view.webview.html = this.getWebviewContent(
+                projectToDisplay,
+                scanToDisplay,
+                isLatestScan,
+                riskManagementResults as { applicationNameIDMap: any[] }
+            );
+            this._view.webview.postMessage({
+                command: "getRiskManagementResults",
+                data: riskManagementResults,
+            });
+            console.log(riskManagementResults);
+        } catch (error) {
+            console.error(error);
+            const errorIcon = this.setWebUri("media", "icons", "error.svg");
+            const styleUri = this.setWebUri("media", "risksManagement.css");
+
+            this._view.webview.html = `<!DOCTYPE html>
+            <html>
+            <head>
+                <link href="${styleUri}" rel="stylesheet">
+            </head>
+            <body>
+                <div class="error-message">
+                    <div>
+                        <img src="${errorIcon}" alt="error" />
+                    </div>feild to get the Checkmarx ASPM results
+                </div>
+            </body>
+            </html>`;
+        } finally {
+            this._view.webview.postMessage({ command: "hideLoader" });
+        }
+
+        //         case applicationNameIDMap not empty but result is empty ,display:
+        //         ASPM does not hold result data for this project
+
+        // case applicationNameIDMap empty ,display:
+        //         this project is not assotiated with any application in the ASPM
+
+    }
+    private setWebUri(...paths: string[]): vscode.Uri {
+        return this._view.webview.asWebviewUri(
+            vscode.Uri.joinPath(this.context.extensionUri, ...paths)
+        );
+    }
+
+    private getWebviewContent(
+        projectName: string,
+        scan: string,
+        isLatestScan: boolean,
+        applications: { applicationNameIDMap: any[] }
+    ): string {
+        const styleResetUri = this.setWebUri("media", "reset.css");
+        const styleVSCodeUri = this.setWebUri("media", "vscode.css");
+        const styleMainUri = this.setWebUri("media", "main.css");
+        const scriptUri = this.setWebUri("media", "risksManagement.js");
+        const styleUri = this.setWebUri("media", "risksManagement.css");
+        const unionIcon = this.setWebUri("media", "icons", "union.svg");
+        const codiconsUri = this.setWebUri(
+            "node_modules",
+            "@vscode/codicons",
+            "dist",
+            "codicon.css"
+        );
+        const popperUri = this.setWebUri(
+            "node_modules",
+            "@popperjs/core",
+            "dist",
+            "umd",
+            "popper.min.js"
+        );
+        const styleBootStrap = this.setWebUri(
+            "media",
+            "bootstrap",
+            "bootstrap.min.css"
+        );
+        const scriptBootStrap = this.setWebUri(
+            "media",
+            "bootstrap",
+            "bootstrap.min.js"
+        );
+        const nonce = getNonce();
+
+        return `<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -124,21 +156,23 @@ export class RisksManagementView implements vscode.WebviewViewProvider {
 </head>
 
 <body>
-	${
-    !projectName || !scan || !isLatestScan
-      ? `<div>
-		ASPM data is only shown when the most recent scan of a project is selected in the Checkmarx One Results tab.
-	</div>`
-      : `<div class="ditales"
-    data-bs-toggle="tooltip" data-bs-placement="top"
+ <div id="loading" class="loading">
+		<div class="spinner-border" role="status">
+		  <span class="visually-hidden">ASPM Results Loadind...</span>
+		</div>
+	  </div>
+      <div id="risksManagementContainer">
+	${!projectName || !scan || !isLatestScan ? `<div class="no-results-message">
+		ASPM data is only shown when the most recent scan of a project is selected in the Checkmarx One Results tab
+	</div>` :
+                `<div class="ditales"
+    data-bs-toggle="tooltip" data-bs-placement="auto"
 	title="You can show ASPM data for a different project by changing the selection in the Checkmarx One Results section above.">
 	
-		<div class="ellipsis"><i class="codicon codicon-project"></i>Project: ${
-      projectName ?? ""
-    }</div>
-		<div class="ellipsis"><i class="codicon codicon-shield"></i>Scan ID: ${
-      scan ?? ""
-    }</div>
+		<div class="ellipsis"><i class="codicon codicon-project"></i>Project: ${projectName ?? ""
+                }</div>
+		<div class="ellipsis"><i class="codicon codicon-shield"></i>Scan ID: ${scan ?? ""
+                }</div>
 	</div>
 
 
@@ -151,16 +185,15 @@ export class RisksManagementView implements vscode.WebviewViewProvider {
 </div>
   
 	<div class="app-header">
-		<img src="${unionIcon}"/> ${
-          applications.applicationNameIDMap.length
-        } Applications
+		<img src="${unionIcon}"/> ${applications.applicationNameIDMap.length
+                } Applications
 	</div>
 	<div class="accordion" id="applicationsContainer"></div>
-	`
-  }
-	<script nonce="${nonce}" src="${scriptUri}">
-		const vscode = acquireVsCodeApi();
+	`}</div>
+	<script nonce="${nonce}">
+    const vscode = acquireVsCodeApi();
 </script>
+<script nonce="${nonce}" src="${scriptUri}"></script>
 <script src=${popperUri}></script>
 
 	<script nonce="${nonce}" src="${scriptBootStrap}"></script>
@@ -168,10 +201,10 @@ export class RisksManagementView implements vscode.WebviewViewProvider {
 </body>
 
 </html>`;
-  }
+    }
 }
 
 function exctarctData(input: string, prefix: string) {
-  const trimmed = input.substring(prefix.length).trim();
-  return trimmed.split(/\s+/)[0];
+    const trimmed = input.substring(prefix.length).trim();
+    return trimmed.split(/\s+/)[0];
 }
