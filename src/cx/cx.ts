6ef46b85dd8a54dd6ea4eeb1c8346248bf71e4ed
Margarital <margarita.levitm@checkmarx.com>
2025-04-14 09:58:34 +0300

    Do a chnage on isScanEnabled and isAIGuidedRemediationEnabled functions

--- src/cx/cx.ts
+++ src/cx/cx.ts
@@ -306,7 +306,13 @@ export class Cx implements CxPlatform {
     if (!token) {
       return enabled;
     }
-    const config = new CxConfig();
+   
+   
+    //const config = new CxConfig();
+    const config = await this.getAstConfiguration();
+    if (!config) {
+      return enabled;
+    }
     config.apiKey = token;
     const cx = new CxWrapper(config);
     try {
@@ -326,7 +332,11 @@ export class Cx implements CxPlatform {
     if (!token) {
       return enabled;
     }
-    const config = new CxConfig();
+   // const config = new CxConfig();
+    const config = await this.getAstConfiguration();
+    if (!config) {
+      return enabled;
+    }
     config.apiKey = token;
     const cx = new CxWrapper(config);
     try {
