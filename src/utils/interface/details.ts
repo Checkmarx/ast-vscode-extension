69a16ec5348a55a550aa59d30598336e823ecf37
Sumit Morchhale <gurjarsumit7898@gmail.com>
2025-06-18 16:18:40 +0530

    fix format

--- src/utils/interface/details.ts
+++ src/utils/interface/details.ts
@@ -135,25 +135,25 @@ export class Details {
 		const ruleNameLower = ruleName.toLowerCase();
 		return `<body>
 				<tr>
-          <td style="background:var(--vscode-editor-background)">
-          <td>
-              <div>
-				<div class="bfl-title">
-				${ruleNameLower} has detected secret for file  
-                        <a href="#" 
-                        class="ast-node"
-                        id="ast-node-0"
-                        data-filename="${this.result.data.filename}" 
-                        data-line="${this.result.data.line}" 
-                        data-column="${0}"
-						data-length="${1}"
-                      >
-				${this.result.data.filename}
-                      </a>
-                    </div>
-                </div>
-              </td>
-            </tr>
+					<td style="background:var(--vscode-editor-background)">
+					<td>
+						<div>
+							<div class="bfl-title">
+							${ruleNameLower} has detected secret for file  
+									<a href="#" 
+									class="ast-node"
+									id="ast-node-0"
+									data-filename="${this.result.data.filename}" 
+									data-line="${this.result.data.line}" 
+									data-column="${0}"
+									data-length="${1}"
+								>
+							${this.result.data.filename}
+								</a>
+								</div>
+							</div>
+						</td>
+				</tr>
 				</body>`;
 	}
 
