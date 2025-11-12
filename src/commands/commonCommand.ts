e7bf436801b35ee91807df70fb7102d719ed5fcf
Rahul Pidde <206018639+cx-rahul-pidde@users.noreply.github.com>
2025-11-12 19:54:35 +0530

    removed space changes

--- src/commands/commonCommand.ts
+++ src/commands/commonCommand.ts
@@ -40,11 +40,11 @@ export class CommonCommand {
 
   public async executeCheckSettings() {
 
-    const isConfiguration = await cx.isValidConfiguration();
+  const isConfiguration = await cx.isValidConfiguration();
     vscode.commands.executeCommand(
       commands.setContext,
       commands.isValidCredentials,
-      isConfiguration
+      isConfiguration 
     );
 
     vscode.commands.executeCommand(
@@ -52,7 +52,7 @@ export class CommonCommand {
       commands.isScaScanEnabled,
       true);
 
-    this.executeCheckScanEnabled();
+     this.executeCheckScanEnabled();
   }
 
   public async executeCheckScanEnabled() {
