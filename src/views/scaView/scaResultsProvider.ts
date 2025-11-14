0f2dd47b444cdab7e21044f21fc90c150b9e6b72
Rahul Pidde <206018639+cx-rahul-pidde@users.noreply.github.com>
2025-11-14 16:02:17 +0530

    renamed file name and function name

--- src/views/scaView/scaResultsProvider.ts
+++ src/views/scaView/scaResultsProvider.ts
@@ -10,7 +10,7 @@ import { messages } from "../../utils/common/messages";
 import { orderResults } from "../../utils/utils";
 import { ResultsProvider } from "../resultsProviders";
 import CxScaRealTimeErrors from "@checkmarxdev/ast-cli-javascript-wrapper/dist/main/scaRealtime/CxScaRealTimeErrors";
-import { validateConfigurationDetails } from "../../utils/common/configGuards";
+import { validateConfigurationAndLicense } from "../../utils/common/configValidators";
 
 export class SCAResultsProvider extends ResultsProvider {
   public process;
@@ -41,7 +41,7 @@ export class SCAResultsProvider extends ResultsProvider {
   }
 
   async refreshData(message?: string): Promise<void> {
-    if (await validateConfigurationDetails(this.logs)) {
+    if (await validateConfigurationAndLicense(this.logs)) {
       this.showStatusBarItem(constants.scaScanRunningLog);
       this.message = message ? message : messages.scaStartScan;
       this.data = this.generateTree().children;
