ee076e2b2968240a2a61ba873ea843f6f5dd2937
Rahul Pidde <206018639+cx-rahul-pidde@users.noreply.github.com>
2025-12-05 14:32:29 +0530

    updated changes

--- src/views/scaView/scaResultsProvider.ts
+++ src/views/scaView/scaResultsProvider.ts
@@ -10,6 +10,7 @@ import { messages } from "../../utils/common/messages";
 import { orderResults } from "../../utils/utils";
 import { ResultsProvider } from "../resultsProviders";
 import CxScaRealTimeErrors from "@checkmarxdev/ast-cli-javascript-wrapper/dist/main/scaRealtime/CxScaRealTimeErrors";
+import { cx } from "../../cx";
 
 export class SCAResultsProvider extends ResultsProvider {
   public process;
@@ -40,11 +41,17 @@ export class SCAResultsProvider extends ResultsProvider {
   }
 
   async refreshData(message?: string): Promise<void> {
-    this.showStatusBarItem(constants.scaScanRunningLog);
-    this.message = message ? message : messages.scaStartScan;
-    this.data = this.generateTree().children;
-    this._onDidChangeTreeData.fire(undefined);
-    this.hideStatusBarItem();
+    if (!await cx.isStandaloneEnabled(this.logs)) {
+      this.showStatusBarItem(constants.scaScanRunningLog);
+      this.message = message ? message : messages.scaStartScan;
+      this.data = this.generateTree().children;
+      this._onDidChangeTreeData.fire(undefined);
+      this.hideStatusBarItem();
+    }
+    else {
+      this.data = [];
+      this._onDidChangeTreeData.fire(undefined);
+    }
   }
 
 
