ea3f40f9f5df697a80e5ceff1de1c588b12bc783
Margarita Levit <45617576+cx-margarita-levitm@users.noreply.github.com>
2026-03-09 13:20:24 +0200

    Fix bug AST-139163 (#1443)
    * Fix bug AST-139163
    ---------
    
    Co-authored-by: Rahul Pidde <206018639+cx-rahul-pidde@users.noreply.github.com>

--- packages/core/src/cx/cxMock.ts
+++ packages/core/src/cx/cxMock.ts
@@ -13,7 +13,6 @@ import { CxCommandOutput } from "@checkmarx/ast-cli-javascript-wrapper/dist/main
 import CxOssResult from "@checkmarx/ast-cli-javascript-wrapper/dist/main/oss/CxOss";
 import CxSecretsResult from "@checkmarx/ast-cli-javascript-wrapper/dist/main/secrets/CxSecrets";
 import CxIacResult from "@checkmarx/ast-cli-javascript-wrapper/dist/main/iacRealtime/CxIac";
-import { FixOutcomeTelemetry } from "../aiTracking/types";
 
 export class CxMock implements CxPlatform {
   private context: vscode.ExtensionContext;
